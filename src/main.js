7f56b309d24b5bd92d1822d49c8fe2f05f48f66d
Lorenayannnnn <lorenayan@qq.com>
2022-04-22 11:08:48 -0700

    init vuesax

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import Axios from "./config/axios";
+import locale from 'element-ui/lib/locale/lang/en'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = Axios
+Vue.use(ElementUI, { locale })
+Vue.use(Vuesax, {
+  // options here
+})
 
 /* eslint-disable no-new */
 new Vue({
